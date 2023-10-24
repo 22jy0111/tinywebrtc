088aabc04c12748c702702bbd0bf360221bb67c2
22jy0111 <22jy0111@jec.ac.jp>
2023-10-24 05:26:38 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { ClientToServerEvents, ServerToClientEvents } from './models';
+
 const app = express();
 const port = 3001;
+const httpServer = createServer(app);
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.emit("hello", "from server");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    socket.on("message", (message) => {
+        console.log(`from client: ${message}`);
+    });
+
+    socket.on("disconnect", (reason) => {
+        console.log(`user disconnected. reason: ${reason}`);
+    });
+});
+
+app.use(express.static('static'));
+
+httpServer.listen(port, () => {
+    console.log(`listening on: ${port}`);
+});
